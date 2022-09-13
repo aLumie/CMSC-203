3fa74c88adb052ee5c1cbb04293b7141f62e6d87
Joseph Sollestre <106940326+aLumie@users.noreply.github.com>
2022-09-13 11:02:14 +0800

    CSMS-203_Activity1

---
+++ App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        String name = "Regina Castillo";
+        System.out.println("Hi, my name is " + name);
+
+        int age = 20;
+        System.out.println("I am " + age + " years old");
+
+        float gpa = 2.25f;
+        System.out.print("My GPA is " + gpa + "\n");
+        
+
+        char bloodtype = 'B';
+        System.out.print("My blood type is " + bloodtype);
+        
+
+    }
+}
