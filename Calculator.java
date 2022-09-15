7f6c54a61ab8cb4536e7f7b0469ec2f7a081dca8
Joseph Sollestre <106940326+aLumie@users.noreply.github.com>
2022-09-15 14:37:56 +0800

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class App{
+
+    static Scanner scanner;
+    public static void main(String[] args) throws Exception {
+        
+        scanner = new Scanner(System.in);
+        double num1, num2, result;
+
+        System.out.println("Input the first number : ");
+        num1 = scanner.nextDouble();
+
+        System.out.println("Input the second number : ");
+        num2 = scanner.nextDouble();
+
+        System.out.print("Result:" + "\n");
+        result = num1 / num2;
+        System.out.println(result);
+    }
+}
