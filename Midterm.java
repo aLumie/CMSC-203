5d660a22a7c1d8c9c10d45305aa9a5dc3195c49a
Joseph Sollestre <106940326+aLumie@users.noreply.github.com>
2022-10-28 15:01:21 +0800

    Add files via upload

---
+++ Midterm.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.util.Scanner;
+
+class Midterm
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	
+		Scanner sc= new Scanner(System.in);
+        System.out.println("Number of Test Cases: ");
+
+		Set<Integer> set=new HashSet<>();
+
+        set.add(5);
+		set.add(6);
+		set.add(7);
+		set.add(13);
+		set.add(14);
+		set.add(20);
+		set.add(21);
+		set.add(27);
+		set.add(28);
+		
+		int t=sc.nextInt();
+		while(t-->0){
+
+		    int n=sc.nextInt();
+		    int count=0;
+
+		    while(n-->0){
+		        int v=sc.nextInt();
+		        if(!set.contains(v))count++;
+		    }
+		    System.out.println(8+count);
+		}
+	}
+}
